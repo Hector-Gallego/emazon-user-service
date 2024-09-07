6786b8e146320f1286f3e024ba5433e3c8f77beb
Hector-Gallego <gallegohector96@gmail.com>
2024-09-06 19:50:47 -0500

    Initial configuration project

---
+++ src/main/java/com/emazon/emazonuserservice/EmazonUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.emazonuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonUserServiceApplication.class, args);
+	}
+
+}
